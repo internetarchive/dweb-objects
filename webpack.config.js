f5a75bde143052b099b3f011f2d02f2beeea92e3
Mitra Ardron <mitra@mitra.biz>
2018-08-16 10:49:54 +1000

    Fix debug in production webpack (same change made on dweb-transports dweb-objects and dweb-archive)

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
     entry: {
         'dweb-objects': './index.js',
@@ -17,6 +18,20 @@ module.exports = {
         Buffer: true,
         setImmediate: false,
         console: false
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    compress: {
+                        unused: false,
+                        collapse_vars: false // debug has a problem in production without this.
+                    }
+
+                    //compress: false  or alternatively remove compression, it only makes about a 5% difference
+                }
+            })
+        ]
     },
     resolve: {
         alias: {
